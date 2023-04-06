1a2d6d2e806c496542f54f3459113ce5b0f43354
digiwhale <digiwhale.humphrey@gmail.com>
2023-04-06 14:24:41 -0400

    1

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-
-// cors
 app.use(cors())
-
 app.use(express.json())
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Astrology api!' }))
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Astrology api!' })
+})
 
-app.get('/example', (req, res) => res.status(200).json({ message: 'Welcome to Example!' }))
+app.get('/example', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Example!' })
+})
 
 app.get('/horoscope', (req, res) => {
   const date = new Date(req.query.time)
@@ -22,8 +23,8 @@ app.get('/horoscope', (req, res) => {
   })
 })
 
-const server = http.createServer(app)
+const port = process.env.PORT || 3001
 
-server.listen(3001, () => {
-  console.log('Server listening on port 3001');
-});
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
