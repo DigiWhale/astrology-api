cfb756bccf5f3c86ac0ecdb1998935589366e25b
digiwhale <digiwhale.humphrey@gmail.com>
2023-04-06 14:22:54 -0400

    1

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-/* eslint-disable no-console */
 const http = require('http')
 const express = require('express')
 const cors = require('cors')
-// const astrologer = require('./astrologer')
 
 const app = express()
+
 // cors
 app.use(cors())
 
 app.use(express.json())
 
-app.get('/', async (req, res) => res.status(200).json({ message: 'Welcome to Astrology api!' }))
+app.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Astrology api!' }))
 
-app.get('/example', async (req, res) => res.status(200).json({ message: 'Welcome to Example!' }))
+app.get('/example', (req, res) => res.status(200).json({ message: 'Welcome to Example!' }))
 
-app.get('/horoscope', async (req, res) => {
+app.get('/horoscope', (req, res) => {
   const date = new Date(req.query.time)
   const { latitude, longitude, houseSystem } = req.query
-
-  // const chart = astrologer.natalChart(date, latitude, longitude, houseSystem)
 
   res.status(200).json({
     data: "chart"
