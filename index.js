56304be6dcc3a8f1dc307bb9d1fc8dc3749176da
digiwhale <digiwhale.humphrey@gmail.com>
2023-04-06 01:58:11 -0400

    1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const astrologer = require('./src/astrologer');
+// const astrologer = require('./src/astrologer');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 
 // const api = require("./src/api");
 // app.use(api);
@@ -15,20 +15,20 @@ app.get("/", async (req, res) =>
 res.send(`Hey this is my API running, on port ${PORT}`)
 );
 
-app.get("/horoscope", async (req, res) => {
-  const date = new Date(req.query.time);
-  const { latitude, longitude, houseSystem } = req.query;
+// app.get("/horoscope", async (req, res) => {
+//   const date = new Date(req.query.time);
+//   const { latitude, longitude, houseSystem } = req.query;
 
-  const chart = astrologer.natalChart(date, latitude, longitude, houseSystem);
+//   const chart = astrologer.natalChart(date, latitude, longitude, houseSystem);
 
-  res.status(200).json({
-    data: chart,
-  });
-});
+//   res.status(200).json({
+//     data: chart,
+//   });
+// });
 
-app.get("/example", async (req, res) => {
-  res.status(200).json({ message: "This is an example route" });
-});
+// app.get("/example", async (req, res) => {
+//   res.status(200).json({ message: "This is an example route" });
+// });
 
 
 app.listen(PORT, () => {
